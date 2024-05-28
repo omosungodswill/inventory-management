13e14389c66242728d68127d9240d191e6b95132
omosungodswill <129052132+omosungodswill@users.noreply.github.com>
2024-05-28 16:43:36 +0100

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+     </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src=";"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
+  <script type="text/javascript" src="libs/js/functions.js"></script>
+  </body>
+</html>
+
+<?php if(isset($db)) { $db->db_disconnect(); } ?>
